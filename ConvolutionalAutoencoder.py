72e2025ad95505c80b49feedfc079fd7061c65f2
sraju728@gmail.com <sraju728@gmail.com>
2018-11-10 16:17:10 +0530

    Create the model of autoencoder

--- ConvolutionalAutoencoder.py
+++ ConvolutionalAutoencoder.py
@@ -91,3 +91,17 @@ print(type(train_labels))
 print('Original label 0 : ', train_labels[0])
 print('After conversion to categorical ( one-hot ) : ', train_labels_one_hot[0])
 
+def autoencoder(input_img):
+	conv1 = Conv2D(32, (3,3), activation='relu', padding='same')(input_img)
+	pool1 = MaxPooling2D(pool_size=(2,2))(conv1)
+	conv2 = Conv2D(64, (3,3), activation='relu', padding='same')(pool1)
+	pool2 = MaxPooling2D(pool_size=(2,2))(conv2)
+	conv3 = Conv2D(128,(3,3), activation='relu', padding='same')(pool2)
+
+
+	conv4 = Conv2D(128,(3,3), activation='relu', padding='same')(conv3)
+	up1 = UpSampling2D((2,2))(conv4)
+	conv5 = Conv2D(64, (3,3), activation='relu', padding='same')(up1)
+	up2 = UpSampling2D((2,2))(conv5)
+	decoded = Conv2D(1, (3,3), activation='sigmoid',padding='same')(up2)
+	return decoded
